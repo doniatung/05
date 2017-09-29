815fda22ef16f12e8e1a5cbed69177053b32a565
doniatung <doniatung32@gmail.com>
2017-09-28 20:31:07 -0400

    updated organization

--- jinjaTraining.py
+++ jinjaTraining.py
@@ -1,49 +1,22 @@
-from flask import Flask
-from flask import render_template
-import random
+from flask import Flask, render_template
+from util import occupations
 
 my_app = Flask(__name__)
 
-def read(csv):
-    thefile = open(csv, 'r')
-    l = thefile.readlines()
-    thefile.close()
-    return l
-#reads the file into a list with each entry as a separate line in the csv
+#print occupations.read('data/occupations.csv')
+dictOfJobs = occupations.makeDictionary(occupations.read('data/occupations.csv'))
 
-def makeDictionary(l):
-    d = {}
-    del l[0]
-    del l[-1]
-    for x in l:
-        if x.count(",") > 1:
-            quote = x.find('"', 1, len(x))
-            k = x[1 : quote]
-            v = float(x[quote + 2: -1])
-            d[k] = v
-        else:
-            comma = x.find(",")
-            k = x[: comma]
-            v = float(x[comma + 1: -1])
-            d[k] = v
-    return d
-#creates a dictionary of the entries in a given list by using the ',' as the separation character between the key and value of the dictionary entry
-
-def pickOne(d):
-    l = []
-    for key in d:
-        v = d[key] * 10
-        counter = 0
-        while counter < v:
-            l.append(key)
-            counter = counter + 1
-    return l[random.randint(0,len(l)-1)]
 
 def htmlOutput():
-    return render_template('hw.html', d = makeDictionary(read("occupations.csv"))) + pickOne(makeDictionary(read("occupations.csv")))
+    return render_template('hw.html', dictionary = dictOfJobs, randJob = occupations.pickOne(dictOfJobs))
+
+@my_app.route("/")
+def root():
+    return "Hey there!" 
+              
 
 @my_app.route("/occupations")
-def root():
+def table():
     return htmlOutput()
 
 if __name__ == '__main__':
